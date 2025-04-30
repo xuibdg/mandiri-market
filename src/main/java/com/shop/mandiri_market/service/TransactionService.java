06e8e997bb98b7279f6461d41be3d98089d491b4
FadhilR <fadilrafq156@gmail.com>
2025-04-30 15:24:17 +0700

    Reporting

--- src/main/java/com/shop/mandiri_market/service/TransactionService.java
+++ src/main/java/com/shop/mandiri_market/service/TransactionService.java
@@ -1,7 +1,14 @@
 package com.shop.mandiri_market.service;
 
 import com.shop.mandiri_market.dto.TransactionRequest;
+import com.shop.mandiri_market.dto.TransactionResponse;
+
+import java.util.List;
 
 public interface TransactionService {
     String createTransaction(TransactionRequest transactionRequest);
+    List<TransactionResponse> getAll(String name);
+    String updateUser(String id, TransactionRequest request);
+    String deletedUser(String id);
+
 }
