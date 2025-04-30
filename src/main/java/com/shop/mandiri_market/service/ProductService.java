a5bba06884722e37dd2c070c79e63fd241a74134
MuhammadAdam31 <muhamadadam113@gmail.com>
2025-04-30 14:03:57 +0700

    add product method create and read

--- src/main/java/com/shop/mandiri_market/service/ProductService.java
+++ src/main/java/com/shop/mandiri_market/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.shop.mandiri_market.service;
 
+import com.shop.mandiri_market.dto.ProductRequest;
+import com.shop.mandiri_market.dto.ProductResponse;
+
+import java.util.List;
+
 public interface ProductService {
+
+    String createProduct(ProductRequest request);
+    List<ProductResponse> getAll(String name);
 }
