294ea8a11cb097f36b29111623de0e2b71857b54
RafieSA <rafiesafaraz@student.telkomuniversity.ac.id>
2025-05-06 03:36:46 +0700

    createTransaction success. add GlobalErrorMapping for exception.

--- src/main/java/com/shop/mandiri_market/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/shop/mandiri_market/service/impl/TransactionServiceImpl.java
@@ -8,10 +8,12 @@ import com.shop.mandiri_market.repository.ProductRepository;
 import com.shop.mandiri_market.repository.ProductStockRepository;
 import com.shop.mandiri_market.repository.TransactionRepository;
 import com.shop.mandiri_market.service.TransactionService;
+import com.shop.mandiri_market.utils.exception.BusinessException;
+import com.shop.mandiri_market.utils.exception.GlobalErrorMapping;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -38,42 +40,41 @@ public class TransactionServiceImpl implements TransactionService {
     ProductStockRepository productStockRepository;
 
     @Override
-    @Transactional
     public String createTransaction(TransactionRequest transactionRequest) {
-        Cashier cashier = cashierRepository.findById(transactionRequest.getCashierId()).orElse(null);
-        Product product = productRepository.findById(transactionRequest.getProductId()).orElse(null);
-        ProductStock productStock = productStockRepository.findByProductId(product.getId()).orElse(null);
+        Cashier cashier = cashierRepository.findById(transactionRequest.getCashierId()).orElseThrow(() -> new BusinessException(HttpStatus.BAD_REQUEST, GlobalErrorMapping.CASHIER_NOT_FOUND));
+        Product product = productRepository.findById(transactionRequest.getProductId()).orElseThrow(() -> new BusinessException(HttpStatus.BAD_REQUEST, GlobalErrorMapping.PRODUCT_ID_NOT_FOUND));
+        ProductStock productStock = productStockRepository.findByProductId(product.getId()).orElseThrow(() -> new BusinessException(HttpStatus.BAD_REQUEST, GlobalErrorMapping.PRODUCT_STOCK_NOT_ENOUGH));
 //        ProductStock productStock = productStockRepository.findById(transactionRequest.getProductId()).orElse(null);
 //        Product product = productRepository.findById(transactionRequest.getProductId()).orElse(null);
 
-        if (productStock.getStock() >= transactionRequest.getQuantity()){
-            BigDecimal totalBuy = product.getPrice().multiply(BigDecimal.valueOf(transactionRequest.getQuantity()));
-            if (transactionRequest.getNominalAmount().compareTo(totalBuy) >= 0) {
-                cashier.setBalance(cashier.getBalance().add(totalBuy));
-                productStock.setStock(productStock.getStock() - transactionRequest.getQuantity());
-                Transaction transaction = Transaction.builder()
-                        .id(UUID.randomUUID().toString())
-                        .cashier(cashier)
-                        .paid(transactionRequest.getNominalAmount())
-                        .totalBuy(totalBuy)
-                        .changeAmount(transactionRequest.getNominalAmount().compareTo(totalBuy) > 0 ? transactionRequest.getNominalAmount().subtract(totalBuy) : BigDecimal.ZERO)
-                        .createdAt(Timestamp.from(Instant.now()))
-                        .status("SUCCESS")
-                        .build();
+        if (productStock.getStock() < transactionRequest.getQuantity()) {
+            throw new BusinessException(HttpStatus.BAD_REQUEST, GlobalErrorMapping.PRODUCT_STOCK_NOT_ENOUGH);
+        }
+        BigDecimal totalBuy = product.getPrice().multiply(BigDecimal.valueOf(transactionRequest.getQuantity()));
+        if (transactionRequest.getNominalAmount().compareTo(totalBuy) >= 0) {
 
-                TransactionDetail transactionDetail = TransactionDetail.builder()
-                        .id(UUID.randomUUID().toString())
-                        .product(product)
-                        .transaction(transaction)
-                        .quantity(transactionRequest.getQuantity())
-                        .createdAt(Timestamp.from(Instant.now()).toLocalDateTime())
-                        .status("SUCCESS")
-                        .build();
-                transaction.setTransactionDetails(List.of(transactionDetail));
-                transactionRepository.save(transaction);
-                cashierRepository.save(cashier);
-                productStockRepository.save(productStock);
-            }
+            cashier.setBalance(cashier.getBalance().add(totalBuy));
+            productStock.setStock(productStock.getStock() - transactionRequest.getQuantity());
+            Transaction transaction = Transaction.builder()
+                    .id(UUID.randomUUID().toString())
+                    .cashier(cashier)
+                    .paid(transactionRequest.getNominalAmount())
+                    .totalBuy(totalBuy)
+                    .changeAmount(transactionRequest.getNominalAmount().compareTo(totalBuy) > 0 ? transactionRequest.getNominalAmount().subtract(totalBuy) : BigDecimal.ZERO)
+                    .createdAt(Timestamp.from(Instant.now()))
+                    .status("SUCCESS")
+                    .build();
+
+            TransactionDetail transactionDetail = TransactionDetail.builder()
+                    .id(UUID.randomUUID().toString())
+                    .product(product)
+                    .transaction(transaction)
+                    .quantity(transactionRequest.getQuantity())
+                    .createdAt(Timestamp.from(Instant.now()).toLocalDateTime())
+                    .status("SUCCESS")
+                    .build();
+            transaction.setTransactionDetails(List.of(transactionDetail));
+            transactionRepository.save(transaction);
         }
         return "Success created transaction";
     }
--- src/main/java/com/shop/mandiri_market/utils/exception/GlobalErrorMapping.java
+++ src/main/java/com/shop/mandiri_market/utils/exception/GlobalErrorMapping.java
@@ -6,9 +6,10 @@ public enum GlobalErrorMapping {
     ERROR("1", "ERROR"),
     DATA_NOT_FOUND_CUSTOM("IEG-0012", "Data ${1} tidak ditemukan. Pastikan Value yang anda masukan sudah sesuai"),
     RULE_NOT_FOUND("IEG-0013", "Data RULE tidak ditemukan. Pastikan Value yang anda masukan sudah sesuai"),
-    CASHIER_NOT_FOUND("IEG-0014", "Data Cashier tidak ditemukan. Pastikan Value yang anda masukan sudah sesuai"),
-    PRODUCT_STOCK_NOT_FOUND("IEG-0015", "Data Product Stock tidak ditemukan. Pastikan Value yang anda masukan sudah sesuai"),
-    PRODUCT_NOT_FOUND("IEG-0016", "Data Product tidak ditemukan. Pastikan Value yang anda masukan sudah sesuai")
+    CASHIER_NOT_FOUND("IEG-0014", "Data Cashier ID tidak ditemukan. Pastikan cashier id yang anda masukan sesuai dengan kondisi di database."),
+    PRODUCT_STOCK_NOT_ENOUGH("IEG-0015", "Stock product yang dipilih, saat ini stock product tersebut kurang dari quantity. Data Product Stock tidak dapat memenuhi permintaan pelanggan."),
+    PRODUCT_ID_NOT_FOUND("IEG-0016", "Data Product ID tidak ditemukan. Pastikan product id yang anda masukan sesuai dengan kondisi di database"),
+    NOMINAL_NOT_ENOUGH("IEG-0017", "Uang pelanggan kurang. Pelanggan tidak dapat membayar harga satuan produk, dikali quantity.")
     ;
 
 
